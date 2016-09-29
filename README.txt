84f6e897cbf9a9b68527029b460d0a0bca1931e6
magresta <magresta@DESKTOP-O9A9JK4.gateway.pace.com>
2016-09-29 12:42:38 -0400

    Initial File Commit for PayPlanner

---
+++ README.txt
@@ -0,0 +1,47 @@
+PayPlanner README
+
+To Do list:
+
+-Fix bug - Cancel button in config.html renders form errors instead of redirecting to home view. Works in dev.
+-Fix bug - Any user can navigate to any budget item but entering /payplanner/config/nnnn - Where nnnn = BudgetItem.id
+		-Shortterm - Top of view compare request.user to item.parentItem.user, if not  match return redirect('home')
+		-Longterm - Create decerator to compare request.user and instance user (e.g instance user would be parentItem.user)
+
+-Extend User object to include categories
+	-models.py 
+		- Create cust_user model, inheriting django.contrib.auth.models.User
+		- Add field user_cat = ManyToMany(Categories,to_field='catName') to cust_user Class 
+		- override Items.__init__ to dyanamically set Categories list to user_cat items
+	-forms.py 
+		- Change model for UserCreateForm to cust_user
+
+-Add password reset link to sign-in page (should be in comments)
+	- Test postfix config is working on server
+
+-Create form for user to manage categories and add categories
+
+-Improve Efficiency/Flow 
+	-Eventually look into getting rid of parent items, build data on income/expense form submit
+		-Would need composite key with name and date to manage editing multiple lines at once.
+		-Not sure if it would be any more efficient.
+
+	-views.py - call update_data under edititem and config, not home load
+
+	-budget.py
+		-Budget.update_data()
+			-Add ability to pass multiple Items object to it, create BudgetData only for that object if they do not exist
+		-Budget.update_line()
+			-Replace parItem = Items.object.get(.... with parItem = line.parentItem -Should work the same and save a db call
+			-Modify parItem (Already  Done), Update BudgetData row (Currently removes it)
+		-Budget.update_future()
+			-Replace parItem = Items.object.get(.... with parItem = line.parentItem -Should work the same and save a db call
+			-Modify Item, Create New parent, Remove BudgetData with parentItem == parItem && effectiveDate > parItem.endDate,
+				pass new parent to update_data 
+
+		-Budget.update_all()
+			-Replace parItem = Items.object.get(.... with parItem = line.parentItem -Should work the same and save a db call
+			-ID what has changed and update all objects on a single line
+
+
+		
+		
---
+++ payplanner/models.py
@@ -0,0 +1,139 @@
+from django.db import models
+from datetime import date
+from django.contrib.auth.models import User
+# Create your models here.
+#Need models to edit expenses and income
+#   Inherit modelforms already created, make Name, paycycle, category uneditable
+#Hold Bills, Expenses and Income
+class Cycles(models.Model):
+    def __str__(self):
+        return self.cycleName
+
+    #Name of Cycle to be displayed on Input Forms
+    cycleName = models.CharField(
+         max_length=150,
+         unique=True,
+         verbose_name='Name'
+    )
+
+    
+    #Cycle Duration
+    cycleLength = models.IntegerField(
+        verbose_name='Length of Cycle(Days)'
+    )
+
+    class Meta:
+        verbose_name_plural = "Pay Cycles"
+        
+#Holds Expense Catgeory Information
+class Categories(models.Model):
+    def __str__(self):
+        return self.catName
+    #Name - To be displayed on Expense Input Form
+    catName = models.CharField(
+        max_length=150,
+        unique=True,
+        verbose_name='Category'
+    )
+    #Description for reports information
+    catDes = models.CharField(
+        max_length=500,
+        unique=True,
+        verbose_name='Description'
+    )
+    class Meta:
+        verbose_name_plural = "Expense/Income Categories"
+        
+#Holds Bills and one time expenses    
+#Add optional End Date!
+class Items(models.Model):
+    def __str__(self):
+        return self.itemName
+    #User who owns item
+    user = models.ForeignKey(User)
+    #Item Name Ex. Car Payment
+    itemName = models.CharField(
+         max_length=150,
+         verbose_name='Name'
+    )
+    #Item type (expense or income)
+    itemType = models.CharField(
+        max_length=20,
+        blank=True,
+        verbose_name = 'Item Type'
+    )
+    #Category of item
+    category = models.ForeignKey(
+        Categories,
+        null=True,
+        blank=True,
+        to_field='catName',
+    )
+    #Ammount Due Ex 9999.99
+    itemAmount = models.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+        verbose_name='Amount Due'
+    )
+    #How Often expense is paid Ex One Time Only, Weekly, Monthly, Quarterly
+    payCycle = models.ForeignKey(
+        Cycles,
+        to_field='cycleName',
+        verbose_name='Pay Cycle',
+    )
+    #Hold day of the month expense is due
+    nextDueDate = models.DateField(
+        default=date.today,
+        verbose_name='Next Due Date',
+    )
+    #Hold end date of expense, not required.
+    #Used if Expense is paid off, Ammount Changes
+    endDate = models.DateField(
+        blank=True,
+        null=True,
+        verbose_name='End Date',
+    )
+
+    #Holds serialize json representing date objects
+    skiplst = models.TextField(
+        blank=True,
+        null=True,
+    )
+    
+    #Holds Notes on expense. Ex. Account Number, Event description
+    itemNote = models.CharField(
+         max_length=400,
+         blank=True,
+         verbose_name='Notes'
+    )
+    class Meta:
+        verbose_name_plural = "Income/Expenses"
+        
+
+
+class BudgetData(models.Model):
+    def __str__(self):
+        display = ('%s %s' % (self.parentItem.itemName,self.effectiveDate))
+        return display
+    parentItem = models.ForeignKey(
+        Items,
+    )
+
+    itemAmmount = models.DecimalField(
+        max_digits=8,
+        decimal_places=2,
+        verbose_name='Amount'
+    )
+    #Hold effective date item
+    effectiveDate = models.DateField(
+        default=date.today,
+    )
+
+    itemNote = models.CharField(
+         max_length=400,
+         blank=True,
+         verbose_name='Notes'
+    )
+
+    class Meta:
+        verbose_name_plural = "Budget Data"
