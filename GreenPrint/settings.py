5fb675af9ef2d6eb72456b19e1b0e5a97400f039
magresta <agresta_matt@yahoo.com>
2016-09-30 15:49:37 -0400

    Added Notes, Supports both dev and prod

--- GreenPrint/settings.py
+++ GreenPrint/settings.py
@@ -1,1 +1,10 @@
-from .settings_dev import *
+#Import Custom Settings
+import sys
+sys.path.insert(0,"/etc/default")
+import pp_settings
+
+#Load settings dependent on enviroment
+if pp_settings.ENVIRONMENT == 'PROD':
+    from .settings_prod import *
+else:
+    from .settings_dev import *
