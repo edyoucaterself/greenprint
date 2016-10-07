df31d38d4467b7cfc1c0109d0046df53177645a3
magresta <agresta_matt@yahoo.com>
2016-10-07 15:18:59 -0400

    Updated to conform to PEP Style Guide

--- payplanner/models.py
+++ payplanner/models.py
@@ -1,11 +1,18 @@
-from django.db import models
+#Forms.py
+# Created By: Matt Agresta
+# Created On: 9/05/2016
+# Holds classes for each payplanner model
+#-----------------------------------------------------------#
 from datetime import date
+
+from django.db import models
 from django.contrib.auth.models import User
-# Create your models here.
-#Need models to edit expenses and income
-#   Inherit modelforms already created, make Name, paycycle, category uneditable
-#Hold Bills, Expenses and Income
+
+
 class Cycles(models.Model):
+    
+    """ Holds pay cycles e.g. Weekly, Bi-Weekly, Monthly """
+    
     def __str__(self):
         return self.cycleName
 
@@ -23,12 +30,17 @@ class Cycles(models.Model):
     )
 
     class Meta:
-        verbose_name_plural = "Pay Cycles"
         
-#Holds Expense Catgeory Information
+        verbose_name_plural = 'Pay Cycles'
+        
+
 class Categories(models.Model):
+
+    """ Holds Expense Catgeory Information """
+    
     def __str__(self):
         return self.catName
+    
     #Name - To be displayed on Expense Input Form
     catName = models.CharField(
         max_length=150,
@@ -41,57 +53,71 @@ class Categories(models.Model):
         unique=True,
         verbose_name='Description'
     )
+    
     class Meta:
-        verbose_name_plural = "Expense/Income Categories"
+        
+        verbose_name_plural = 'Expense/Income Categories'
         
 
 class UserCat(models.Model):
+
+    """ Holds List of Categories User would like on expenses form """
+    
     user = models.OneToOneField(User)
     cats = models.ManyToManyField(Categories,
-                                  verbose_name="Select Desired Categories")
+                                  verbose_name='Select Desired Categories')
 
     
-#Holds Bills and one time expenses    
-#Add optional End Date!
 class Items(models.Model):
+
+    """ Holds Expense and Income information for user """
+    
     def __str__(self):
         return self.itemName
+    
     #User who owns item
     user = models.ForeignKey(User)
+    
     #Item Name Ex. Car Payment
     itemName = models.CharField(
          max_length=150,
          verbose_name='Name'
     )
+    
     #Item type (expense or income)
     itemType = models.CharField(
         max_length=20,
         blank=True,
         verbose_name = 'Item Type'
     )
+    
     #Category of item
     category = models.CharField(
         max_length=100,
         null=True,
         blank=True,
     )
+    
     #Ammount Due Ex 9999.99
     itemAmount = models.DecimalField(
         max_digits=6,
         decimal_places=2,
         verbose_name='Amount'
     )
+    
     #How Often expense is paid Ex One Time Only, Weekly, Monthly, Quarterly
     payCycle = models.ForeignKey(
         Cycles,
         to_field='cycleName',
         verbose_name='Pay Cycle',
     )
+    
     #Hold day of the month expense is due
     nextDueDate = models.DateField(
         default=date.today,
         verbose_name='Next Due Date',
     )
+    
     #Hold end date of expense, not required.
     #Used if Expense is paid off, Ammount Changes
     endDate = models.DateField(
@@ -112,19 +138,27 @@ class Items(models.Model):
          blank=True,
          verbose_name='Notes (optional)'
     )
+    
     class Meta:
-        verbose_name_plural = "Income/Expenses"
+        
+        verbose_name_plural = 'Income/Expenses'
         
 
 
 class BudgetData(models.Model):
+
+    """ Holds line items for budget """
+    
     def __str__(self):
         display = ('%s %s' % (self.parentItem.itemName,self.effectiveDate))
         return display
+
+    #Item object used to create line
     parentItem = models.ForeignKey(
         Items,
     )
 
+    #Amount of line item
     itemAmmount = models.DecimalField(
         max_digits=8,
         decimal_places=2,
@@ -135,6 +169,7 @@ class BudgetData(models.Model):
         default=date.today,
     )
 
+    #Any additional information for line item
     itemNote = models.CharField(
          max_length=400,
          blank=True,
@@ -142,5 +177,6 @@ class BudgetData(models.Model):
     )
 
     class Meta:
-        verbose_name_plural = "Budget Data"
+        
+        verbose_name_plural = 'Budget Data'
 
