452735786ff40c6f45549a3265926f0024bf2e74
magresta <agresta_matt@yahoo.com>
2016-10-21 09:21:22 -0400

    Added base fixture

--- GreenPrint/settings_prod.py
+++ GreenPrint/settings_prod.py
@@ -9,6 +9,10 @@ https://docs.djangoproject.com/en/1.8/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.8/ref/settings/
 """
+#fixtute location
+FIXTURE_DIRS = (
+   '/app/GreenPrint/payplanner/fixtures/',
+)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
